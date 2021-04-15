3b226b1991aeb120b0779cd1e00c3847e8c93e8f
Shrut Sarswat <shrutsarswat@Shruts-MacBook-Pro.local>
2021-04-15 20:04:52 +0530

    Initial Commit

---
+++ TestCode.py
@@ -0,0 +1,7 @@
+number1= input ("enter number 1: " ,)
+number2= input ("enter number 2: ",)
+if number1 > number2:
+    print(f' {number1} is grater than {number2}')
+else :
+    print(f' {number2} is greater than {number1}')
+
